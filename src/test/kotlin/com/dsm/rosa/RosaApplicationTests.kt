a43fdf9a4910a8989b88a326ac28c1af59d3e6a8
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-03-24 19:06:31 +0900

    Project Environment Setting.

---
+++ src/test/kotlin/com/dsm/rosa/RosaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dsm.rosa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RosaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
