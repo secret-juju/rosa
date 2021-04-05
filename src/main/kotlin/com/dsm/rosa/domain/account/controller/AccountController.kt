6be8afc8d8892b2dcb693af1b84a576ad4416b91
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-05 15:50:43 +0900

    Add account controller.

--- src/main/kotlin/com/dsm/rosa/domain/account/controller/AccountController.kt
+++ src/main/kotlin/com/dsm/rosa/domain/account/controller/AccountController.kt
@@ -1,4 +1,14 @@
 package com.dsm.rosa.domain.account.controller
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class AccountController {
+
+    @GetMapping("/user")
+    fun test(@AuthenticationPrincipal principal: AuthenticationPrincipal) {
+
+    }
 }
