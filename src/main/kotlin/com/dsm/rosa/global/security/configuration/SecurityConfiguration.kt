45a356c4e923c12cc201686ce435bf2fed11b63f
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-05 15:57:39 +0900

    create security configuration about google oauth2.

--- src/main/kotlin/com/dsm/rosa/global/security/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/com/dsm/rosa/global/security/configuration/SecurityConfiguration.kt
@@ -1,4 +1,23 @@
 package com.dsm.rosa.global.security.configuration
 
-class SecurityConfiguration {
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+            .antMatchers("/", "/oauth2/**", "/login/**", "/console/**").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .oauth2Login()
+            .and()
+                .exceptionHandling()
+                    .authenticationEntryPoint(LoginUrlAuthenticationEntryPoint("/login"))
+    }
 }
