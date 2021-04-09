d4d775ed5fb43ce06216fdedf185a73eb49b756b
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:02:36 +0900

    Implemented google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/global/security/provider/AuthenticationProvider.kt
+++ src/main/kotlin/com/dsm/rosa/global/security/provider/AuthenticationProvider.kt
@@ -1,24 +1,15 @@
 package com.dsm.clematis.global.security.provider
 
-import com.dsm.clematis.domain.account.domain.Account
-import com.dsm.clematis.domain.account.exception.AccountNotFoundException
-import com.dsm.clematis.domain.account.repository.AccountRepository
-import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.data.repository.findByIdOrNull
+import com.dsm.rosa.domain.account.repository.AccountRepository
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
+import javax.security.auth.login.AccountNotFoundException
 
 @Component
 class AuthenticationProvider(
     private val accountRepository: AccountRepository,
 ) : UserDetailsService {
 
-    override fun loadUserByUsername(username: String) =
-        accountRepository.findByIdOrNull(username) ?: throw AccountNotFoundException(username)
-
-    fun getAccountIdByAuthentication() =
-        (SecurityContextHolder.getContext().authentication.principal as Account).id
-
-    fun getAccountName() =
-        (SecurityContextHolder.getContext().authentication.principal as Account).name
+    override fun loadUserByUsername(email: String) =
+        accountRepository.findByEmail(email) ?: throw AccountNotFoundException(email)
 }
