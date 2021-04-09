3cb316f991fec936a6a9fe69c72c738e808607bf
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:09:15 +0900

    Implemented google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/global/configuration/ConverterConfiguration.kt
+++ src/main/kotlin/com/dsm/rosa/global/configuration/ConverterConfiguration.kt
@@ -1,4 +1,14 @@
 package com.dsm.rosa.global.configuration
 
-class ConverterConfiguration {
+import com.dsm.rosa.global.converter.OAuthTypeConverter
+import org.springframework.context.annotation.Configuration
+import org.springframework.format.FormatterRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class ConverterConfiguration : WebMvcConfigurer {
+
+    override fun addFormatters(registry: FormatterRegistry) {
+        registry.addConverter(OAuthTypeConverter())
+    }
 }
