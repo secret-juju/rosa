a237ec14b3ea0e624273ddc912c6e2517e01d90b
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:06:08 +0900

    Implemented google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/global/security/exception/InvalidTokenException.kt
+++ src/main/kotlin/com/dsm/rosa/global/security/exception/InvalidTokenException.kt
@@ -1,4 +1,9 @@
 package com.dsm.rosa.global.security.exception
 
-class InvalidTokenException {
-}
+import org.springframework.http.HttpStatus
+
+class InvalidTokenException : CommonException(
+    errorCode = "INVALID_TOKEN",
+    errorMessage = "토큰이 잘못되었습니다.",
+    errorStatus = HttpStatus.UNAUTHORIZED,
+)
