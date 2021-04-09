e9a68ea71dd295cfdc4b1c14bd705e390ef58884
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:14:53 +0900

    Implemented google oauth2 authentication.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 //    implementation("org.springframework.security.oauth:spring-security-oauth2")
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
+
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
