21e52f83bc26c83a976b1739881a7256d5f87e57
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:05:47 +0900

    Implemented google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/global/security/exception/UnsupportedOAuth2TypeException.kt
+++ src/main/kotlin/com/dsm/rosa/global/security/exception/UnsupportedOAuth2TypeException.kt
@@ -1,4 +1,11 @@
 package com.dsm.rosa.global.security.exception
 
-class UnsupportedOAuth2TypeException {
-}
+import org.springframework.http.HttpStatus
+
+class UnsupportedOAuth2TypeException(
+    oauth2Type: String,
+) : CommonException(
+    errorCode = "UNSUPPORTED_OAUTH2_TYPE",
+    errorMessage = "지원하지 않는 OAuth2 타입입니다. [$oauth2Type]",
+    errorStatus = HttpStatus.BAD_REQUEST,
+)
