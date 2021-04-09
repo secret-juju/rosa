c5a4d1ae9677f0335a411966df04dee3ef554b13
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:10:12 +0900

    Implemented google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/domain/account/service/AuthenticationService.kt
+++ src/main/kotlin/com/dsm/rosa/domain/account/service/AuthenticationService.kt
@@ -1,4 +1,17 @@
 package com.dsm.rosa.domain.account.service
 
-class AuthenticationService {
+import com.dsm.rosa.global.attribute.Token
+import com.dsm.rosa.global.security.provider.TokenProvider
+import org.springframework.stereotype.Service
+
+@Service
+class AuthenticationService(
+    val tokenProvider: TokenProvider,
+) {
+
+    fun createAccessToken(accountEmail: String) =
+        tokenProvider.createToken(accountEmail, Token.ACCESS)
+
+    fun createRefreshToken(accountEmail: String) =
+        tokenProvider.createToken(accountEmail, Token.REFRESH)
 }
