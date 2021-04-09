bad5262d13fa7de36fc72643fec333e3795afd3a
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:14:26 +0900

    Implemented google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/domain/account/controller/AccountController.kt
+++ src/main/kotlin/com/dsm/rosa/domain/account/controller/AccountController.kt
@@ -1,4 +1,33 @@
 package com.dsm.rosa.domain.account.controller
 
-class AccountController {
+import com.dsm.rosa.domain.account.controller.response.LoginResponse
+import com.dsm.rosa.domain.account.service.AccountRegistrationService
+import com.dsm.rosa.domain.account.service.AuthenticationService
+import com.dsm.rosa.global.attribute.OAuth2Type
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class AccountController(
+    private val accountRegistrationService: AccountRegistrationService,
+    private val authenticationService: AuthenticationService,
+) {
+
+    @PostMapping("/login/oauth2/code/{oauth2-type}")
+    fun login(
+        @RequestHeader("oauth2-token")
+        oAuth2Token: String,
+        @PathVariable("oauth2-type")
+        oAuth2Type: OAuth2Type,
+    ): LoginResponse {
+        val accountInformation = accountRegistrationService.registerAccount(
+            oAuth2Token = oAuth2Token,
+            oAuth2Type = oAuth2Type,
+        )
+
+        return LoginResponse(
+            accessToken = authenticationService.createAccessToken(accountInformation.accountEmail),
+            refreshToken = authenticationService.createRefreshToken(accountInformation.accountEmail),
+            accountName = accountInformation.accountName,
+        )
+    }
 }
