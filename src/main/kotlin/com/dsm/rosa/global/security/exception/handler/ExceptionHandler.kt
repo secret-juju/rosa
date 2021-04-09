febb90fd0c7748643f4ad54fd23ab62e0e54d448
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:04:44 +0900

    Implemented google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/global/security/exception/handler/ExceptionHandler.kt
+++ src/main/kotlin/com/dsm/rosa/global/security/exception/handler/ExceptionHandler.kt
@@ -1,16 +1,14 @@
-package com.dsm.clematis.global.exception.handler
+package com.dsm.rosa.global.security.exception.handler
 
-import com.dsm.clematis.global.exception.CommonException
-import com.dsm.clematis.global.exception.response.CommonExceptionResponse
+import com.dsm.rosa.global.security.exception.CommonException
+import com.dsm.rosa.global.security.exception.response.ExceptionResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
-import org.springframework.security.core.AuthenticationException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import javax.validation.ConstraintViolationException
 
 @RestControllerAdvice
 class ExceptionHandler {
@@ -18,26 +16,26 @@ class ExceptionHandler {
     @ExceptionHandler(CommonException::class)
     fun commonExceptionHandler(e: CommonException) =
         ResponseEntity(
-            CommonExceptionResponse(
-                code = e.code,
-                message = e.message?: "알 수 없는 오류",
+            ExceptionResponse(
+                errorCode = e.errorCode,
+                errorMessage = e.message?: "알 수 없는 오류",
             ),
-            e.status,
+            e.errorStatus,
         )
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun methodArgumentNotValidExceptionHandler(e: MethodArgumentNotValidException) =
-        CommonExceptionResponse(
-            code = "INVALID_REQUEST",
-            message = e.bindingResult.fieldError?.defaultMessage ?: "알 수 없는 에러",
+        ExceptionResponse(
+            errorCode = "INVALID_REQUEST",
+            errorMessage = e.bindingResult.fieldError?.defaultMessage ?: "알 수 없는 에러",
         )
 
     @ExceptionHandler(HttpMessageNotReadableException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun httpMessageNotReadableExceptionHandler(e: HttpMessageNotReadableException) =
-        CommonExceptionResponse(
-            code = "INVALID_JSON",
-            message = "JSON 형식이 잘못되었습니다.",
+        ExceptionResponse(
+            errorCode = "INVALID_JSON",
+            errorMessage = "JSON 형식이 잘못되었습니다.",
         )
 }
