08595d9be5a91d8b7195e233199d7fe7b2acaafb
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:13:53 +0900

    Implemented google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/domain/account/domain/Account.kt
+++ src/main/kotlin/com/dsm/rosa/domain/account/domain/Account.kt
@@ -1,4 +1,31 @@
 package com.dsm.rosa.domain.account.domain
 
-class Account {
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import javax.persistence.*
+
+@Entity
+@Table(name = "account")
+class Account(
+
+    @Column(name = "email")
+    val email: String,
+
+    @Column(name = "name")
+    val name: String,
+) : UserDetails {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    override fun getAuthorities() = null
+    override fun getPassword() = null
+    override fun getUsername() = email
+
+    override fun isAccountNonExpired() = true
+    override fun isAccountNonLocked() = true
+    override fun isCredentialsNonExpired() = true
+    override fun isEnabled() = true
 }
