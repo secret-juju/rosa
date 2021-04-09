6730f3597049e53941e1f4c70eef04daf748d1e6
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:09:53 +0900

    Implemented google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/global/attribute/OAuth2Type.kt
+++ src/main/kotlin/com/dsm/rosa/global/attribute/OAuth2Type.kt
@@ -1,7 +1,7 @@
 package com.dsm.rosa.global.attribute
 
-enum class OAuthType {
-    GOOGLE,
-    FACEBOOK,
-    NAVER,
+enum class OAuth2Type(val value: String) {
+    GOOGLE("google"),
+    FACEBOOK("facebook"),
+    NAVER("naver"),
 }
