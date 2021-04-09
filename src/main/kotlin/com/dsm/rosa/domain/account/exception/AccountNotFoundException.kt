29be2f51f5841ca662d4060f34776018341f02fd
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:13:39 +0900

    Implemented google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/domain/account/exception/AccountNotFoundException.kt
+++ src/main/kotlin/com/dsm/rosa/domain/account/exception/AccountNotFoundException.kt
@@ -1,4 +1,12 @@
 package com.dsm.rosa.domain.account.exception
 
-class AccountNotFoundException {
-}
+import com.dsm.rosa.global.security.exception.CommonException
+import org.springframework.http.HttpStatus
+
+class AccountNotFoundException(
+    accountEmail: String,
+) : CommonException(
+    errorCode = "ACCOUNT_NOT_FOUND",
+    errorMessage = "계정을 찾을 수 없습니다. [$accountEmail]",
+    errorStatus = HttpStatus.NOT_FOUND,
+)
