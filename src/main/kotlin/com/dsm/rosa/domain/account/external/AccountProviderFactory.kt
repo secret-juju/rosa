9a86f473551709be8c93b9382d6ef2663cd8ef1a
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:12:32 +0900

    Implemented google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/domain/account/external/AccountProviderFactory.kt
+++ src/main/kotlin/com/dsm/rosa/domain/account/external/AccountProviderFactory.kt
@@ -1,5 +1,21 @@
 package com.dsm.rosa.domain.account.external
 
-class AccountSearchServiceFactory {
+import com.dsm.rosa.global.attribute.OAuth2Type
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
 
+object AccountProviderFactory {
+    private val oAuth2Connection = Retrofit.Builder()
+        .baseUrl("https://www.googleapis.com")
+        .addConverterFactory(JacksonConverterFactory.create(jacksonObjectMapper()))
+        .build()
+        .create(AccountProviderConnection::class.java)
+
+    fun getAccountProvider(oAuth2Type: OAuth2Type) =
+        when (oAuth2Type) {
+            OAuth2Type.GOOGLE -> GoogleAccountProvider(oAuth2Connection)
+            OAuth2Type.FACEBOOK -> throw Exception()
+            OAuth2Type.NAVER -> throw Exception()
+        }
 }
