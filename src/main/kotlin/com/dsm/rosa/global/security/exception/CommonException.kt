f6846debe9e90afb7239c861b473937324edb82e
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-09 23:06:26 +0900

    Implemented google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/global/security/exception/CommonException.kt
+++ src/main/kotlin/com/dsm/rosa/global/security/exception/CommonException.kt
@@ -1,4 +1,9 @@
 package com.dsm.rosa.global.security.exception
 
-class CommonException {
-}
+import org.springframework.http.HttpStatus
+
+open class CommonException(
+    val errorCode: String,
+    private val errorMessage: String,
+    val errorStatus: HttpStatus,
+) : RuntimeException(errorMessage)
