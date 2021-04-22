04c2146f5d617c837f03bb0b75ac080f48529d0e
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-22 15:12:11 +0900

    solved unresolved reference.

--- src/main/kotlin/com/dsm/rosa/domain/account/external/provider/GoogleAccountProvider.kt
+++ src/main/kotlin/com/dsm/rosa/domain/account/external/provider/GoogleAccountProvider.kt
@@ -1,6 +1,6 @@
 package com.dsm.rosa.domain.account.external.provider
 
-import com.dsm.rosa.domain.account.external.AccountProviderConnection
+import com.dsm.rosa.domain.account.external.connection.AccountProviderConnection
 import com.dsm.rosa.domain.account.external.response.OAuth2AuthenticationResponse
 import com.dsm.rosa.global.security.exception.InvalidTokenException
 
