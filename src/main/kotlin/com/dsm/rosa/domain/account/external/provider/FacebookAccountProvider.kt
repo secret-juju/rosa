a35fdeb02336f3cb03e37848a74cbde1670091f3
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-22 14:50:37 +0900

    add provider that facebook.

--- src/main/kotlin/com/dsm/rosa/domain/account/external/provider/FacebookAccountProvider.kt
+++ src/main/kotlin/com/dsm/rosa/domain/account/external/provider/FacebookAccountProvider.kt
@@ -1,4 +1,17 @@
-package com.dsm.rosa.domain.account.external
+package com.dsm.rosa.domain.account.external.provider
 
-class FacebookAccountProvider {
+import com.dsm.rosa.domain.account.external.connection.AccountProviderConnection
+import com.dsm.rosa.domain.account.external.response.OAuth2AuthenticationResponse
+
+class FacebookAccountProvider(
+    private val accountProviderConnection: AccountProviderConnection,
+) : AccountProvider {
+
+    override fun searchAccount(oAuth2Token: String): OAuth2AuthenticationResponse {
+
+        return OAuth2AuthenticationResponse(
+            accountId = "",
+            accountName = "",
+        )
+    }
 }
