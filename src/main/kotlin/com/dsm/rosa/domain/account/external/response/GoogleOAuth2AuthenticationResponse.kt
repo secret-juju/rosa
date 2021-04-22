3e59ef3a043e5bdbbdcde626d2c337af7724db45
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-22 14:48:37 +0900

    add response that google oauth2 authentication.

--- src/main/kotlin/com/dsm/rosa/domain/account/external/response/GoogleOAuth2AuthenticationResponse.kt
+++ src/main/kotlin/com/dsm/rosa/domain/account/external/response/GoogleOAuth2AuthenticationResponse.kt
@@ -1,4 +1,4 @@
-package com.dsm.rosa.domain.account.external
+package com.dsm.rosa.domain.account.external.response
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
