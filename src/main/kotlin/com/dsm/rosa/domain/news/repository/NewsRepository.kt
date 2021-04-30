5d316ed01228f64014be4a8d0724572b8d0569b0
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-30 11:32:03 +0900

    new repository.

--- src/main/kotlin/com/dsm/rosa/domain/news/repository/NewsRepository.kt
+++ src/main/kotlin/com/dsm/rosa/domain/news/repository/NewsRepository.kt
@@ -1,4 +1,8 @@
 package com.dsm.rosa.domain.news.repository
 
-interface NewsRepository {
+import com.dsm.rosa.domain.news.domain.News
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface NewsRepository : JpaRepository<News, Long> {
+    fun findByCompanyTickerSymbol(companyTickerSymbol: String): List<News>
 }
