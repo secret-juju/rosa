f99e22d708e6cffb2ca42260b7d93ef1c5f67254
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-30 11:33:06 +0900

    add search bookmark service.

--- src/main/kotlin/com/dsm/rosa/domain/bookmark/service/BookmarkSearchService.kt
+++ src/main/kotlin/com/dsm/rosa/domain/bookmark/service/BookmarkSearchService.kt
@@ -1,4 +1,18 @@
 package com.dsm.rosa.domain.bookmark.service
 
-class BookmarkSearchService {
+import com.dsm.rosa.domain.bookmark.repository.BookmarkRepository
+import org.springframework.stereotype.Service
+
+@Service
+class BookmarkSearchService(
+    private val bookmarkRepository: BookmarkRepository,
+) {
+
+    fun isExistBookmark(
+        accountEmail: String,
+        companyTickerSymbol: String,
+    ) = bookmarkRepository.existsByAccountEmailAndCompanyTickerSymbol(
+        accountEmail = accountEmail,
+        companyTickerSymbol = companyTickerSymbol,
+    )
 }
