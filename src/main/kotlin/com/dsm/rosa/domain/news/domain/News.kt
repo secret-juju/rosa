bdf324744e42d0b81d0a59172a84f15978ae0936
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-30 11:32:25 +0900

    add news domain.

--- src/main/kotlin/com/dsm/rosa/domain/news/domain/News.kt
+++ src/main/kotlin/com/dsm/rosa/domain/news/domain/News.kt
@@ -1,4 +1,28 @@
 package com.dsm.rosa.domain.news.domain
 
-class News {
+import com.dsm.rosa.domain.company.domain.Company
+import java.time.LocalDate
+import javax.persistence.*
+
+@Entity
+@Table(name = "news")
+class News(
+    @Column(name = "content")
+    val content: String,
+
+    @Column(name = "positivity")
+    val positivity: Double,
+
+    @Column(name = "published_date")
+    val publishedDate: LocalDate,
+
+    @ManyToOne
+    @JoinColumn(name = "company_id", referencedColumnName = "id")
+    val company: Company,
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long? = null
 }
