3bca4cb15991a95de6d580fe0f7e9f85e0be55ff
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-30 11:31:45 +0900

    add stock controller.

--- src/main/kotlin/com/dsm/rosa/domain/stock/controller/StockController.kt
+++ src/main/kotlin/com/dsm/rosa/domain/stock/controller/StockController.kt
@@ -1,4 +1,42 @@
 package com.dsm.rosa.domain.stock.controller
 
-class StockController {
+import com.dsm.rosa.domain.bookmark.service.BookmarkSearchService
+import com.dsm.rosa.domain.stock.controller.response.StockDetailResponse
+import com.dsm.rosa.domain.stock.service.StockSearchService
+import com.dsm.rosa.global.security.provider.AuthenticationProvider
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+@RestController
+@RequestMapping("/stock")
+@Validated
+class StockController(
+    private val stockSearchService: StockSearchService,
+    private val bookmarkSearchService: BookmarkSearchService,
+    private val authenticationProvider: AuthenticationProvider,
+) {
+
+    @GetMapping("/{companyTickerSymbol}")
+    fun searchStockDetail(
+        @Size(min = 6, max = 6)
+        @NotBlank
+        @PathVariable("companyTickerSymbol")
+        companyTickerSymbol: String,
+    ) = StockDetailResponse(
+        stock = stockSearchService.getStockDetailByCompany(
+            companyTickerSymbol = companyTickerSymbol,
+        ),
+        news = stockSearchService.getNewsByCompany(
+            companyTickerSymbol = companyTickerSymbol,
+        ),
+        isBookmarked = bookmarkSearchService.isExistBookmark(
+            accountEmail = authenticationProvider.getAccountEmail(),
+            companyTickerSymbol = companyTickerSymbol,
+        ),
+    )
 }
