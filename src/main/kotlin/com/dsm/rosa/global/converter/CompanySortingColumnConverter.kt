4d9af23334194c20c1f0b5a7f4f3461a25e6ef8c
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-01 22:24:06 +0900

    add converter that company sorting column.

--- src/main/kotlin/com/dsm/rosa/global/converter/CompanySortingColumnConverter.kt
+++ src/main/kotlin/com/dsm/rosa/global/converter/CompanySortingColumnConverter.kt
@@ -1,4 +1,14 @@
 package com.dsm.rosa.global.converter
 
-class CompanySortingColumnConverter {
+import com.dsm.rosa.global.attribute.CompanySortingColumn
+import com.dsm.rosa.global.exception.NonExistCompanySortingColumnException
+import org.springframework.core.convert.converter.Converter
+
+class CompanySortingColumnConverter : Converter<String, CompanySortingColumn> {
+
+    override fun convert(request: String) =
+        CompanySortingColumn
+            .values()
+            .singleOrNull { request == it.smallLetter }
+            ?: throw NonExistCompanySortingColumnException(request)
 }
