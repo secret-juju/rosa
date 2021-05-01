42cbbdc357ad7eefd1fdf49a46e53ec5bb340389
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-01 22:22:32 +0900

    add exception that non exist company sorting column.

--- src/main/kotlin/com/dsm/rosa/global/exception/NonExistCompanySortingColumnException.kt
+++ src/main/kotlin/com/dsm/rosa/global/exception/NonExistCompanySortingColumnException.kt
@@ -1,4 +1,11 @@
 package com.dsm.rosa.global.exception
 
-class NonExistCompanySortingColumnException {
-}
+import org.springframework.http.HttpStatus
+
+class NonExistCompanySortingColumnException(
+    companySortingColumn: String,
+) : CommonException(
+    code = "NON_EXIST_SORTING_COLUMN",
+    message = "존재하지 않는 회사 정렬 컬럼입니다. [$companySortingColumn]",
+    status = HttpStatus.BAD_REQUEST,
+)
