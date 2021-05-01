1a038c84a76214fd9002a47260011f8b59ff2bab
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-01 22:24:29 +0900

    add query dsl configuration.

--- src/main/kotlin/com/dsm/rosa/global/configuration/QueryDslConfiguration.kt
+++ src/main/kotlin/com/dsm/rosa/global/configuration/QueryDslConfiguration.kt
@@ -1,4 +1,17 @@
 package com.dsm.rosa.global.configuration
 
-class QueryDslConfiguration {
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Configuration
+class QueryDslConfiguration(
+    @PersistenceContext
+    val entityManager: EntityManager,
+) {
+
+    @Bean
+    fun jpaQueryFactory() = JPAQueryFactory(entityManager)
 }
