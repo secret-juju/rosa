78dd29f4810ffeb6d46fe9efa0a2af63f167426d
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-03 14:07:36 +0900

    CompanySortingColumn was renamed to CompanySortingCondition.

--- src/main/kotlin/com/dsm/rosa/global/converter/CompanySortingColumnConverter.kt
+++ src/main/kotlin/com/dsm/rosa/global/converter/CompanySortingColumnConverter.kt
@@ -1,13 +1,13 @@
 package com.dsm.rosa.global.converter
 
-import com.dsm.rosa.global.attribute.CompanySortingColumn
+import com.dsm.rosa.global.attribute.CompanySortingCondition
 import com.dsm.rosa.global.exception.NonExistCompanySortingColumnException
 import org.springframework.core.convert.converter.Converter
 
-class CompanySortingColumnConverter : Converter<String, CompanySortingColumn> {
+class CompanySortingColumnConverter : Converter<String, CompanySortingCondition> {
 
     override fun convert(request: String) =
-        CompanySortingColumn
+        CompanySortingCondition
             .values()
             .singleOrNull { request == it.smallLetter }
             ?: throw NonExistCompanySortingColumnException(request)
