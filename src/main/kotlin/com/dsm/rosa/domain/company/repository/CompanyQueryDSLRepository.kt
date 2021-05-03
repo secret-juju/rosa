e720338506a678e5c220ee302abef07d9a416624
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-03 18:13:21 +0900

    remove CompanyDTO.

--- src/main/kotlin/com/dsm/rosa/domain/company/repository/CompanyQueryDSLRepository.kt
+++ src/main/kotlin/com/dsm/rosa/domain/company/repository/CompanyQueryDSLRepository.kt
@@ -1,22 +1,16 @@
 package com.dsm.rosa.domain.company.repository
 
-import com.dsm.rosa.domain.company.domain.Company
 import com.dsm.rosa.domain.company.domain.QCompany
-import com.dsm.rosa.domain.company.repository.dto.SortingCompanyDto
-import com.dsm.rosa.domain.news.domain.News
 import com.dsm.rosa.domain.news.domain.QNews
 import com.dsm.rosa.domain.stock.domain.QStock
-import com.dsm.rosa.domain.stock.domain.Stock
 import com.dsm.rosa.global.attribute.CompanySortingCondition
 import com.dsm.rosa.global.attribute.CompanySortingMethod
-import com.querydsl.core.types.Projections
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
 import org.springframework.data.domain.SliceImpl
 import org.springframework.stereotype.Repository
 import java.time.LocalDate
-import kotlin.math.roundToLong
 
 @Repository
 class CompanyQueryDSLRepository(
