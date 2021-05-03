99285543fb593b10190709322bb61d890e0c8687
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-03 14:46:11 +0900

    change the return value due to a change in the CompanySearchService.

--- src/main/kotlin/com/dsm/rosa/domain/company/controller/CompanyController.kt
+++ src/main/kotlin/com/dsm/rosa/domain/company/controller/CompanyController.kt
@@ -1,16 +1,12 @@
 package com.dsm.rosa.domain.company.controller
 
-import com.dsm.rosa.domain.company.controller.response.MultipleCompanyResponse
-import com.dsm.rosa.domain.company.repository.CompanyQueryDSLRepository
 import com.dsm.rosa.domain.company.service.CompanySearchService
-import com.dsm.rosa.domain.stock.exception.StockNotFoundException
 import com.dsm.rosa.global.attribute.CompanySortingCondition
 import com.dsm.rosa.global.attribute.CompanySortingMethod
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
 
 @RestController
 class CompanyController(
@@ -24,11 +20,9 @@ class CompanyController(
         @RequestParam("sorting-method")
         sortingMethod: CompanySortingMethod,
         pageInformation: Pageable,
-    ) = MultipleCompanyResponse(
-        companies = companySearchService.searchCompany(
-            pageInformation = pageInformation,
-            sortingCondition = sortingCondition,
-            sortingMethod = sortingMethod,
-        )
+    ) = companySearchService.searchCompany(
+        pageInformation = pageInformation,
+        sortingCondition = sortingCondition,
+        sortingMethod = sortingMethod,
     )
 }
