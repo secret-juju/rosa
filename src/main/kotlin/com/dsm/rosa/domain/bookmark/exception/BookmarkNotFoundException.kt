3dcea3c9206e6df3f78638210b972e5b3334c534
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-04 11:45:10 +0900

    add exception that bookmark not found and alread exist.

--- src/main/kotlin/com/dsm/rosa/domain/bookmark/exception/BookmarkNotFoundException.kt
+++ src/main/kotlin/com/dsm/rosa/domain/bookmark/exception/BookmarkNotFoundException.kt
@@ -1,4 +1,13 @@
 package com.dsm.rosa.domain.bookmark.exception
 
-class BookmarkNotFoundException {
-}
+import com.dsm.rosa.global.exception.CommonException
+import org.springframework.http.HttpStatus
+
+class BookmarkNotFoundException(
+    accountEmail: String,
+    companyTickerSymbol: String,
+) : CommonException(
+    code = "BOOKMARK_NOT_FOUND",
+    message = "$accountEmail 유저는 종목코드가 ${companyTickerSymbol}인 회사를 즐겨찾기 하지 않았습니다.",
+    status = HttpStatus.NOT_FOUND,
+)
