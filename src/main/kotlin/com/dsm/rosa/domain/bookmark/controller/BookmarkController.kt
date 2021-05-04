56d8f5c46e11ec22da9555358631226f159d61c6
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-04 11:45:27 +0900

    add bookmark controller.

--- src/main/kotlin/com/dsm/rosa/domain/bookmark/controller/BookmarkController.kt
+++ src/main/kotlin/com/dsm/rosa/domain/bookmark/controller/BookmarkController.kt
@@ -1,4 +1,39 @@
 package com.dsm.rosa.domain.bookmark.controller
 
-class BookmarkController {
+import com.dsm.rosa.domain.bookmark.service.BookmarkCreationService
+import com.dsm.rosa.domain.bookmark.service.BookmarkDeletionService
+import com.dsm.rosa.global.security.provider.AuthenticationProvider
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class BookmarkController(
+    private val bookmarkCreationService: BookmarkCreationService,
+    private val bookmarkDeletionService: BookmarkDeletionService,
+    private val authenticationProvider: AuthenticationProvider,
+) {
+
+    @PostMapping("/company/{companyTickerSymbol}/bookmark")
+    @ResponseStatus(HttpStatus.OK)
+    fun subscribeCompany(
+        @PathVariable("companyTickerSymbol")
+        companyTickerSymbol: String,
+    ) {
+        bookmarkCreationService.applyBookmark(
+            accountEmail = authenticationProvider.getAccountEmail(),
+            companyTickerSymbol = companyTickerSymbol,
+        )
+    }
+
+    @DeleteMapping("/company/{companyTickerSymbol}/bookmark")
+    @ResponseStatus(HttpStatus.OK)
+    fun unsubscribeCompany(
+        @PathVariable("companyTickerSymbol")
+        companyTickerSymbol: String,
+    ) {
+        bookmarkDeletionService.removeBookmark(
+            accountEmail = authenticationProvider.getAccountEmail(),
+            companyTickerSymbol = companyTickerSymbol,
+        )
+    }
 }
