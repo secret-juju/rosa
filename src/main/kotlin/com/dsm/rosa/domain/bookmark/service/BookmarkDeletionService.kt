4384980a2422925b1fa73f5daefb8d63975b97d5
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-04 11:43:14 +0900

    add services to turn off bookmark.

--- src/main/kotlin/com/dsm/rosa/domain/bookmark/service/BookmarkDeletionService.kt
+++ src/main/kotlin/com/dsm/rosa/domain/bookmark/service/BookmarkDeletionService.kt
@@ -1,4 +1,32 @@
 package com.dsm.rosa.domain.bookmark.service
 
-class BookmarkDeletionService {
+import com.dsm.rosa.domain.bookmark.exception.BookmarkNotFoundException
+import com.dsm.rosa.domain.bookmark.repository.BookmarkRepository
+import org.springframework.stereotype.Service
+
+@Service
+class BookmarkDeletionService(
+    private val bookmarkRepository: BookmarkRepository,
+) {
+
+    fun removeBookmark(accountEmail: String, companyTickerSymbol: String) =
+        if (isAlreadyApplyBookmark(accountEmail, companyTickerSymbol))
+            deleteBookmark(
+                accountEmail = accountEmail,
+                companyTickerSymbol = companyTickerSymbol,
+            )
+        else
+            throw BookmarkNotFoundException(accountEmail, companyTickerSymbol)
+
+    private fun isAlreadyApplyBookmark(accountEmail: String, companyTickerSymbol: String) =
+        bookmarkRepository.existsByAccountEmailAndCompanyTickerSymbol(
+            accountEmail = accountEmail,
+            companyTickerSymbol = companyTickerSymbol,
+        )
+
+    private fun deleteBookmark(accountEmail: String, companyTickerSymbol: String) =
+        bookmarkRepository.deleteByAccountEmailAndCompanyTickerSymbol(
+            accountEmail = accountEmail,
+            companyTickerSymbol = companyTickerSymbol,
+        )
 }
