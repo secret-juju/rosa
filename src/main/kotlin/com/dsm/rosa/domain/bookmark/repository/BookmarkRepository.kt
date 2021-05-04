a623231f87458920a45a4ccd2d5ad6967ab0f6b8
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-04 11:44:33 +0900

    adding repository to add and delete bookmark.

--- src/main/kotlin/com/dsm/rosa/domain/bookmark/repository/BookmarkRepository.kt
+++ src/main/kotlin/com/dsm/rosa/domain/bookmark/repository/BookmarkRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface BookmarkRepository : JpaRepository<Bookmark, Long> {
     fun existsByAccountEmailAndCompanyTickerSymbol(accountEmail: String, companyTickerSymbol: String): Boolean
+    fun deleteByAccountEmailAndCompanyTickerSymbol(accountEmail: String, companyTickerSymbol: String)
 }
