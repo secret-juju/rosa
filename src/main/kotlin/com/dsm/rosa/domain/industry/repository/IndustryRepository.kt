36f37015a1b0cf44b25b985928fff1796d44d1f2
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-05 16:05:54 +0900

    소스 TTS복사하기필기인식기

--- src/main/kotlin/com/dsm/rosa/domain/industry/repository/IndustryRepository.kt
+++ src/main/kotlin/com/dsm/rosa/domain/industry/repository/IndustryRepository.kt
@@ -1,4 +1,12 @@
 package com.dsm.rosa.domain.industry.repository
 
-interface IndustryRepository {
+import com.dsm.rosa.domain.industry.domain.Industry
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
+import org.springframework.data.jpa.repository.EntityGraph
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface IndustryRepository : JpaRepository<Industry, Long> {
+    @EntityGraph(attributePaths = ["company", "company.stocks", "company.news"])
+    fun findByName(companyIndustryName: String, pageable: Pageable): Slice<Industry>
 }
