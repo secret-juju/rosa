1014be6cfa66365df7154a814954068bfd2df16f
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-06 14:19:43 +0900

    develop an API to reissue access tokens.

--- src/main/kotlin/com/dsm/rosa/domain/account/controller/AccountController.kt
+++ src/main/kotlin/com/dsm/rosa/domain/account/controller/AccountController.kt
@@ -1,15 +1,24 @@
 package com.dsm.rosa.domain.account.controller
 
+import com.dsm.rosa.domain.account.controller.request.TokenRequest
 import com.dsm.rosa.domain.account.controller.response.LoginResponse
+import com.dsm.rosa.domain.account.controller.response.TokenResponse
 import com.dsm.rosa.domain.account.service.AccountRegistrationService
-import com.dsm.rosa.domain.account.service.AuthenticationService
+import com.dsm.rosa.domain.account.service.AuthenticationCreationService
+import com.dsm.rosa.domain.account.service.AuthenticationSearchService
+import com.dsm.rosa.domain.account.service.AuthenticationValidationService
 import com.dsm.rosa.global.attribute.OAuth2Type
+import com.dsm.rosa.global.attribute.Token
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+import javax.validation.constraints.NotNull
 
 @RestController
 class AccountController(
     private val accountRegistrationService: AccountRegistrationService,
-    private val authenticationService: AuthenticationService,
+    private val authenticationCreationService: AuthenticationCreationService,
+    private val authenticationValidationService: AuthenticationValidationService,
+    private val authenticationSearchService: AuthenticationSearchService,
 ) {
 
     @PostMapping("/login/oauth2/code/{oauth2-type}")
@@ -25,9 +34,30 @@ class AccountController(
         )
 
         return LoginResponse(
-            accessToken = authenticationService.createAccessToken(accountInformation.accountId),
-            refreshToken = authenticationService.createRefreshToken(accountInformation.accountId),
+            accessToken = authenticationCreationService.createAccessToken(accountInformation.accountId),
+            refreshToken = authenticationCreationService.createRefreshToken(accountInformation.accountId),
             accountName = accountInformation.accountName,
+        )
+    }
+
+    @PostMapping("/auth/token")
+    fun reissueAccessToken(
+        @NotNull
+        @Valid
+        @RequestBody
+        request: TokenRequest,
+    ): TokenResponse {
+        authenticationValidationService.validateBothToken(
+            accessToken = request.accessToken,
+            refreshToken = request.refreshToken,
+        )
+
+        return TokenResponse(
+            token = authenticationCreationService.createAccessToken(
+                accountEmail = authenticationSearchService.getAuthenticationData(
+                    token = request.accessToken
+                )
+            )
         )
     }
 }
