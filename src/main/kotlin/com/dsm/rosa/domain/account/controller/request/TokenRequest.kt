218915ab9f9e9a7f5c40ae583abe8e77dea41f33
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-06 14:19:10 +0900

    add token request.

--- src/main/kotlin/com/dsm/rosa/domain/account/controller/request/TokenRequest.kt
+++ src/main/kotlin/com/dsm/rosa/domain/account/controller/request/TokenRequest.kt
@@ -1,4 +1,10 @@
 package com.dsm.rosa.domain.account.controller.request
 
-class TokenRequest {
-}
+import javax.validation.constraints.NotBlank
+
+data class TokenRequest(
+    @get:NotBlank(message = "<NULL> <EMPTY> <BLANK>")
+    val accessToken: String,
+    @get:NotBlank(message = "<NULL> <EMPTY> <BLANK>")
+    val refreshToken: String,
+)
