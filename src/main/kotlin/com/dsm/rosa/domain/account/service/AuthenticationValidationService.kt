acf50871770158bc059684fe96acead7fd2fc21c
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-06 14:17:21 +0900

    add AuthenticationValidationService to verify authenticated information.

--- src/main/kotlin/com/dsm/rosa/domain/account/service/AuthenticationValidationService.kt
+++ src/main/kotlin/com/dsm/rosa/domain/account/service/AuthenticationValidationService.kt
@@ -1,4 +1,32 @@
 package com.dsm.rosa.domain.account.service
 
-class AuthenticationValidationService {
+import com.dsm.rosa.global.attribute.Token
+import com.dsm.rosa.global.exception.InvalidTokenException
+import com.dsm.rosa.global.security.provider.TokenProvider
+import org.springframework.stereotype.Service
+
+@Service
+class AuthenticationValidationService(
+    private val tokenProvider: TokenProvider,
+) {
+
+    fun validateBothToken(accessToken: String, refreshToken: String) {
+        val isValidAccessToken = validateToken(accessToken, Token.ACCESS)
+        val isValidRefreshToken = validateToken(refreshToken, Token.REFRESH)
+
+        if (!(isValidAccessToken && isValidRefreshToken))
+            throw InvalidTokenException()
+    }
+
+    fun validateToken(token: String, tokenType: Token): Boolean {
+        val isValidToken = tokenProvider.isToken(
+            token = token,
+            tokenType = tokenType,
+        )
+
+        if (!isValidToken)
+            throw InvalidTokenException()
+
+        return isValidToken
+    }
 }
