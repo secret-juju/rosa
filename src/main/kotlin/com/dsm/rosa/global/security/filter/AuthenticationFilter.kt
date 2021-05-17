d24cfd6c2254b06f3ea9f15a2b2aa34d4360ac77
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-17 22:11:16 +0900

    add account not found exception.

--- src/main/kotlin/com/dsm/rosa/global/security/filter/AuthenticationFilter.kt
+++ src/main/kotlin/com/dsm/rosa/global/security/filter/AuthenticationFilter.kt
@@ -18,7 +18,7 @@ class AuthenticationFilter(
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
-        filterChain: FilterChain
+        filterChain: FilterChain,
     ) {
         val token = tokenProvider.extractToken(request)
 
